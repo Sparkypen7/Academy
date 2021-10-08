06e99618d3f53c62396f92af3216086f70d049de
Sparkypen7 <pasharuiz@hotmail.com>
2021-10-08 11:52:46 -0400

    Built and published by Moonwave

---
+++ assets/js/7784a3a0.d86fec7b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/UpliftGames/moonwave/blob/master","baseUrl":"/Luau-Academy/","classOrder":[]}')}}]);
