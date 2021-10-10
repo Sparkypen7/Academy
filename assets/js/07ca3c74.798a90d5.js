1b64d560756514d42f2960c769a624b4e40897a2
Sparkypen7 <pasharuiz@hotmail.com>
2021-10-10 16:13:01 -0400

    Built and published by Moonwave

---
+++ assets/js/07ca3c74.798a90d5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[120],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/Sparkypen7/Luau-Crash-Course/blob/master","baseUrl":"/Academy/","classOrder":[]}')}}]);
---
+++ assets/js/b062ea49.f00bd23e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[798],{82824:function(s){s.exports=JSON.parse('[{"type":"link","href":"/api/MyFirstClass","label":"MyFirstClass"}]')}}]);
